2346fb6230f8bdbdddfac385ef295c644fee7e6a
John Melati <github@jcmelati.com>
2024-08-06 18:57:50 +0200

    fix: github CI

--- modules/services/src/main/kotlin/com/sphereon/oid/fed/services/KeyService.kt
+++ modules/services/src/main/kotlin/com/sphereon/oid/fed/services/KeyService.kt
@@ -7,17 +7,18 @@ import com.sphereon.oid.fed.persistence.models.Jwk
 import com.sphereon.oid.fed.services.extensions.toJwkDTO
 
 class KeyService {
+    private val accountRepository = Persistence.accountRepository
     private val keyRepository = Persistence.keyRepository
 
     fun create(accountUsername: String): Jwk {
-        val account = Persistence.accountRepository.findByUsername(accountUsername) ?: throw IllegalArgumentException("Account not found")
+        val account = accountRepository.findByUsername(accountUsername) ?: throw IllegalArgumentException("Account not found")
         val accountId = account.id
         val key = generateKeyPair()
         return keyRepository.create(accountId, key)
     }
 
     fun getKeys(accountUsername: String): List<JwkDto> {
-        val account = Persistence.accountRepository.findByUsername(accountUsername) ?: throw IllegalArgumentException("Account not found")
+        val account = accountRepository.findByUsername(accountUsername) ?: throw IllegalArgumentException("Account not found")
         val accountId = account.id
         return keyRepository.findByAccountId(accountId).map { it.toJwkDTO() }
     }
