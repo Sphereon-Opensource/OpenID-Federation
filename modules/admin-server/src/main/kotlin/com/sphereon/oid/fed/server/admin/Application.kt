0ef977de2173f0bab7c948f26f7cfcf0b85a56e8
Robert Mathew <robert.c.mathew@gmail.com>
2024-08-09 17:04:51 +0530

    fix: Linking service module

--- modules/admin-server/src/main/kotlin/com/sphereon/oid/fed/server/admin/Application.kt
+++ modules/admin-server/src/main/kotlin/com/sphereon/oid/fed/server/admin/Application.kt
@@ -2,8 +2,10 @@ package com.sphereon.oid.fed.server.admin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.sphereon.oid.fed.services"])
 class Application
 
 fun main(args: Array<String>) {
