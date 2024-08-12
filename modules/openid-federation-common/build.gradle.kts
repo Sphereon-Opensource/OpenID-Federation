7a58f32fa40095309ec40c6ac08c17030708d6d3
John Melati <github@jcmelati.com>
2024-08-12 12:44:01 +0200

    fix: disable android build

--- modules/openid-federation-common/build.gradle.kts
+++ modules/openid-federation-common/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+//    alias(libs.plugins.androidLibrary)
     kotlin("plugin.serialization") version "2.0.0"
 }
 
