eb112120ba5e9ecb611d2cf01c774d551dceeb07
John Melati <github@jcmelati.com>
2024-08-22 15:13:55 +0200

    fix: table name case

--- modules/persistence/src/commonMain/kotlin/com/sphereon/oid/fed/persistence/Persistence.kt
+++ modules/persistence/src/commonMain/kotlin/com/sphereon/oid/fed/persistence/Persistence.kt
@@ -1,6 +1,11 @@
 package com.sphereon.oid.fed.persistence
 
-import com.sphereon.oid.fed.persistence.models.*
+import com.sphereon.oid.fed.persistence.models.AccountQueries
+import com.sphereon.oid.fed.persistence.models.AuthorityHintQueries
+import com.sphereon.oid.fed.persistence.models.EntityConfigurationMetadataQueries
+import com.sphereon.oid.fed.persistence.models.EntityConfigurationStatementQueries
+import com.sphereon.oid.fed.persistence.models.KeyQueries
+import com.sphereon.oid.fed.persistence.models.SubordinateQueries
 
 expect object Persistence {
     val entityConfigurationStatementQueries: EntityConfigurationStatementQueries
@@ -8,4 +13,5 @@ expect object Persistence {
     val keyQueries: KeyQueries
     val subordinateQueries: SubordinateQueries
     val entityConfigurationMetadataQueries: EntityConfigurationMetadataQueries
+    val authorityHintQueries: AuthorityHintQueries
 }
