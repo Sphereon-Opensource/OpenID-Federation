e9147e72ec0a9e9318d4f558fe9c3085809cad2d
Robert Mathew <robert.c.mathew@gmail.com>
2024-09-02 17:11:36 +0530

    fix: Added signing for EntityConfigurationStatement

--- modules/services/src/commonMain/kotlin/com/sphereon/oid/fed/services/EntityConfigurationStatementService.kt
+++ modules/services/src/commonMain/kotlin/com/sphereon/oid/fed/services/EntityConfigurationStatementService.kt
@@ -4,14 +4,17 @@ import com.sphereon.oid.fed.common.builder.EntityConfigurationStatementBuilder
 import com.sphereon.oid.fed.common.builder.FederationEntityMetadataBuilder
 import com.sphereon.oid.fed.openapi.models.EntityConfigurationStatement
 import com.sphereon.oid.fed.openapi.models.FederationEntityMetadata
+import com.sphereon.oid.fed.openapi.models.JWTHeader
 import com.sphereon.oid.fed.persistence.Persistence
 import com.sphereon.oid.fed.services.extensions.toJwkDTO
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 
 class EntityConfigurationStatementService {
     private val accountService = AccountService()
     private val keyService = KeyService()
+    private val kmsService = KmsService("local")
     private val entityConfigurationStatementQueries = Persistence.entityConfigurationStatementQueries
     private val accountQueries = Persistence.accountQueries
     private val subordinateQueries = Persistence.subordinateQueries
@@ -64,12 +67,19 @@ class EntityConfigurationStatementService {
 
         val entityConfigurationStatement = findByUsername(accountUsername)
 
-        // @TO-DO JWT creation and signing
+        val entityConfigurationStatementStr = Json.encodeToString(entityConfigurationStatement)
+        val entityConfigurationStatementObject = Json.parseToJsonElement(entityConfigurationStatementStr).jsonObject
+        val key = "key_id"
+        val jwt = kmsService.sign(
+            payload = entityConfigurationStatementObject,
+            header = JWTHeader(typ = "entity-statement+jwt"),
+            keyId = key
+        )
 
         entityConfigurationStatementQueries.create(
             account_id = account.id,
             expires_at = entityConfigurationStatement.exp.toLong(),
-            statement = Json.encodeToString(EntityConfigurationStatement.serializer(), entityConfigurationStatement)
+            statement = jwt
         ).executeAsOne()
 
         return entityConfigurationStatement
