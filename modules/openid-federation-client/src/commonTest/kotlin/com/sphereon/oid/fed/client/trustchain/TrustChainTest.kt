5d203ddb2e233fd9e3fd3c2901d452ddba31fa9a
John Melati <github@jcmelati.com>
2024-10-08 23:44:22 +0200

    fix: extend trust chain build test

--- modules/openid-federation-client/src/commonTest/kotlin/com/sphereon/oid/fed/client/trustchain/TrustChainTest.kt
+++ modules/openid-federation-client/src/commonTest/kotlin/com/sphereon/oid/fed/client/trustchain/TrustChainTest.kt
@@ -31,5 +31,44 @@ class TrustChainTest {
 
         assertNotNull(trustChain)
         assertEquals(trustChain.size, 4)
+
+        assertEquals(
+            trustChain[0],
+            mockResponses[Url("https://spid.wbss.it/Spid/oidc/rp/ipasv_lt/.well-known/openid-federation")]
+        )
+
+        assertEquals(
+            trustChain[1],
+            mockResponses[Url("https://spid.wbss.it/Spid/oidc/sa/fetch?sub=https://spid.wbss.it/Spid/oidc/rp/ipasv_lt")]
+        )
+
+        assertEquals(
+            trustChain[2],
+            mockResponses[Url("https://oidc.registry.servizicie.interno.gov.it/fetch?sub=https://spid.wbss.it/Spid/oidc/sa")]
+        )
+        assertEquals(
+            trustChain[3],
+            mockResponses[Url("https://oidc.registry.servizicie.interno.gov.it/.well-known/openid-federation")]
+        )
+
+        val trustChain2 = trustChainService.resolveTrustChain(
+            "https://spid.wbss.it/Spid/oidc/sa",
+            arrayOf("https://oidc.registry.servizicie.interno.gov.it")
+        )
+
+        assertNotNull(trustChain2)
+        assertEquals(trustChain2.size, 3)
+        assertEquals(
+            trustChain2[0],
+            mockResponses[Url("https://spid.wbss.it/Spid/oidc/sa/.well-known/openid-federation")]
+        )
+        assertEquals(
+            trustChain2[1],
+            mockResponses[Url("https://oidc.registry.servizicie.interno.gov.it/fetch?sub=https://spid.wbss.it/Spid/oidc/sa")]
+        )
+        assertEquals(
+            trustChain2[2],
+            mockResponses[Url("https://oidc.registry.servizicie.interno.gov.it/.well-known/openid-federation")]
+        )
     }
 }
