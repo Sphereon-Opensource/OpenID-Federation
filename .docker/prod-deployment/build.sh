40e6ff49b3e25248d9908dd5eb2aa501fbce9c3d
sanderPostma <spostma@sphereon.com>
2024-11-04 23:05:09 +0100

    chore: docker prod-deployment fixes

--- .docker/prod-deployment/build.sh
+++ .docker/prod-deployment/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source ./version-config.sh
+if ! source ./version-config.sh; then
+    echo "Error: Failed to source version-config.sh"
+    exit 1
+fi
 
 docker build -t ${FED_IMAGE}:${FED_VERSION} -f ../federation-server/Dockerfile ../../
-docker build -t ${ADMIN_IMAGE}:${ADMIN_VERSION} -f ../admin-server/Dockerfile ../../
+docker build -t ${ADMIN_IMAGE}:${FED_VERSION} -f ../admin-server/Dockerfile ../../
