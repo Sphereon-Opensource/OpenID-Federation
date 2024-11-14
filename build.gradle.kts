d84ef9f2962813a1258346df95be842e6881faa1
John Melati <github@jcmelati.com>
2024-11-14 14:02:44 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.1.0-SNAPSHOT"
+    version = "0.1.1-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
     // Common repository configuration for all projects
