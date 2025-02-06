24e60c5e4fc18a60a88cfb7ce0207ed84c2a564f
John Melati <github@jcmelati.com>
2025-02-06 12:24:51 +0100

    chore: add header auth to endpoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.6.1-SNAPSHOT"
+    version = "0.6.2-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
     // Common repository configuration for all projects
