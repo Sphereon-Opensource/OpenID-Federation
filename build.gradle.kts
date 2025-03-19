e199eb2a773c60c9ff237f49cc89b138b322778b
Niels Klomp <nklomp@sphereon.com>
2025-03-19 16:22:52 +0100

    docs: environment variable changes and documentation of them

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.5.6-SNAPSHOT"
+    version = "0.5.7-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
 
