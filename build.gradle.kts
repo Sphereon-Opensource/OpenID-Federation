58bb1d91967d7d80873758c23bd92ea2cebff24e
Niels Klomp <nklomp@sphereon.com>
2025-03-19 16:31:32 +0100

    feat: Move docker compose files to start using the .env(.local) files for env vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.10.0-SNAPSHOT"
+    version = "0.10.1-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
 
