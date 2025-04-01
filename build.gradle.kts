1969667a2446255b1303771ed5ac0b3f75de99dd
John Melati <github@jcmelati.com>
2025-04-01 23:14:03 +0200

    fix: jar reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.12.2-SNAPSHOT"
+    version = "0.12.3-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
 
