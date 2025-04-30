ba4bd8c579a28c25888102a310befd6feae0f56f
John Melati <github@jcmelati.com>
2025-04-30 09:58:45 +0200

    chore: adjust version

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,7 +97,7 @@ fun getNpmVersion(): String {
 
 allprojects {
     group = "com.sphereon.oid.fed"
-    version = "0.20.14-SNAPSHOT"
+    version = "0.20.12-SNAPSHOT"
     val npmVersion by extra { getNpmVersion() }
 
     configurations {
